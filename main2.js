5c6dfa2a68654a5be3f248f50284a3658f5554d4
NiklasSorensen <niklass137@gmail.com>
2017-09-14 17:42:34 +0200

    Main 2 er tilføjet
    Forsøgte at køre to nodes

--- main2.js
+++ main2.js
@@ -1,11 +1,11 @@
 
-console.log('Starting main.js');
+console.log('Starting main2.js');
 // modules
 const http = require("http");
 // js files
 const nodeCreator = require('./nodeCreator');
 // variables
-var newNode = new nodeCreator(8080);
+var newNode = new nodeCreator(8081);
 var ID = 'ID:' + newNode.ID;
 var port = '\nport:' + newNode.port;
 
