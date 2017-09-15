8f6323bfc0c3cdacab49ae53545fdf1acca96322
Christian Maigaard <maigaard.christian@gmail.com>
2017-09-15 14:09:31 +0200

    something

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ var port = newNode.port;
 
 http.createServer(function(req,res){
 	res.writeHeader(200,{'Content-Type': 'text/html'});
-	res.write("<h1>Kademilia</h1>");
+	res.write("<h1>Kademlia</h1>");
 	res.write("<h3>ID </h3>"+ ID + "<h3>Port </h3>" + port);
 	res.end();
 }).listen(newNode.port);
