ca3a68cb0117d8d5665b108777a68fc62206c9f1
NiklasSorensen <niklass137@gmail.com>
2017-09-18 16:12:35 +0200

    ID - random 8 bit binary

--- idGenerator.js
+++ idGenerator.js
@@ -1,10 +1,31 @@
 
 
+
 const sha1 = require('sha1');
 
 var newID = function createID(){
-	var rN = Math.random();
-	var ID = parseInt(sha1(rN).substring(0,9),16);
+	//var rN = Math.random();
+	//var ID = parseInt(sha1(rN).substring(0,9),16);
+	var ID = leftPad(randomIntInc(0, 255).toString(2),8);
 	return ID;
 }
+
+function randomIntInc (low, high) {
+    return Math.floor(Math.random() * (high - low + 1) + low);
+}
+
+function leftPad (str, length) {
+    str = str == null ? '' : String(str);
+    length = ~~length;
+    pad = '';
+    padLength = length - str.length;
+
+    while(padLength--) {
+        pad += '0';
+    }
+
+    return pad + str;
+}
+
+
 module.exports = newID;
