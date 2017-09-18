a9f6f6a5da504ca220ecad7c2ba0be62d78f998a
NiklasSorensen <niklass137@gmail.com>
2017-09-18 20:53:10 +0200

    Tilføjet exports til kBucketManager

--- kBucketManager.js
+++ kBucketManager.js
@@ -4,7 +4,7 @@ var xorID = require("./XOR");
 
 //Denne metode returnere et nummer, som er tilsvarende den KBucket, som sortedID skal placeres i.
 
-function kBucketManager(managerId,sortedId){
+var kBucketManager = function kBucketManager(managerId,sortedId){
 
 	this.sortedId = sortedId; // her skal anvendes det ID'et for den node som skal sorteres.
 	this.managerId = managerId; //her skal anvendes det ID'et fra den node som bruger metoden
@@ -20,7 +20,7 @@ function kBucketManager(managerId,sortedId){
 	return 8; //8 kunne i dette tilfælde blivet tolket, som værende et ID'erne er ens
 }
 
-function updateBucket(bucket, newNodeId){
+var updateBucket = function updateBucket(bucket, newNodeId){
 	
 	this.bucket = bucket;
 	this.newNodeId = newNodeId;
@@ -28,13 +28,22 @@ function updateBucket(bucket, newNodeId){
 	for(i=0; i<bucket.length; i++){
 		if(bucket[i] == newNodeId){
 			//placer den nye/kendte node i bagerst i arrayet
+
+			//returner et updateret array
 		}else if(bucket.length >= 7){ //7 skal i dette tilfalde være K,altså antallet af mulige kontaker i en KBucket
 			//PING den første node i arrayet, hvis noden svarer, ignorer den nye node
 			//hvis den ikke svarer, så smid den ud og tilføj den nye bagerst i arrayet
+		
+			//returner et updateret array
 		}else{
-			//Placer nye node bagerst i arrayet	
+			//Placer nye node bagerst i arrayet
+
+			//returner et updateret array	
 		}
 	}
 
 
 }
+
+module.exports = kBucketManager;
+module.exports = updateBucket;
